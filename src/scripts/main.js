3248091c31230f6c932b2bd79cd741b68c9f1aa6
Anna Józefowicz <anna.jozefowicz.86@gmail.com>
2019-11-19 10:29:50 +0100

    fixed navigation, added bg image with custom background-position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,39 @@
 
 $(document).ready(function(){
 
-  $("#theTarget").skippr({
+  $("#theTarget").skippr();
 
-  });
-
+  if (window.innerWidth < 576) {
+    $('.hero').css({ height: window.innerHeight });
+  }
 });    
-if (window.innerWidth < 576) {
-  $('.hero').css({ height: window.innerHeight });
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.menu-wrapper');
+
+const toggleNav = () => {
+  hamburger.classList.toggle('hamburger--active');
+  nav.classList.toggle('menu-wrapper--active');
 }
+
+hamburger.addEventListener('click', toggleNav);
+nav.addEventListener('click', toggleNav);
+
+const searchBtn = document.querySelector('.search__btn-js');
+const searchForm = document.querySelector('.search__form');
+
+const toggleSearch = () => {
+  searchForm.classList.toggle('search__form--active');
+}
+
+searchBtn.addEventListener('click', toggleSearch);
+
+// prevent navigation animation trigger when resizing window
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.   body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
